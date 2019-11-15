b46d0a58c838b9cebd583ca8fe74b77411b3b38f
kj <koolj.net@gmail.com>
2019-11-15 12:25:21 +0700

    First commit

---
+++ components/shared/Layout.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import Head from 'next/head'
+
+class Layout extends Component {
+
+  render () {
+    return (
+      <div>
+        <Head>
+          <title>Miraway MasaNano</title>
+          <meta charSet='utf-8' />
+          <meta
+            name='viewport'
+            content='width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1,user-scalable=0,initial-scale=1'
+          />
+          <script type="text/javascript" src="/static/js/fabric.min.js" />
+          <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+          <link rel="icon" href="favicon.ico" type="image/x-icon">
+        </Head>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+export default Layout
